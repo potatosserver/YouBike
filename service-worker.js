419fec44ab7f9352229bfe5bc880398b83e4fa14
potatosserver <potatosserver@gmail.com>
2025-06-25 18:17:59 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -6,28 +6,16 @@ self.addEventListener('install', (event) => {
                 '/YouBike/index.html',
                 '/YouBike/main.js',
                 '/YouBike/manifest.json',
-                '/YouBike/icons/icon-192x192.png',
-                '/YouBike/icons/icon-512x512.png',
-                '/YouBike/icons/icon.ico',
-                '/YouBike/icons/icon.jpg',
-                '/YouBike/icons/github.png',
-                '/YouBike/icons/github.webp',
-                '/YouBike/icons/html.png',
-                '/YouBike/icons/html.webp',
-                '/YouBike/icons/python.png',
-                '/YouBike/icons/python.webp',
-                '/YouBike/icons/translate.png',
-                '/YouBike/icons/translate.webp',
-                '/YouBike/icons/install.png',
-                '/YouBike/icons/install.webp',
                 'https://unpkg.com/leaflet/dist/leaflet.css',
                 'https://unpkg.com/leaflet/dist/leaflet.js',
                 'https://unpkg.com/leaflet.markercluster/dist/leaflet.markercluster.js',
                 'https://unpkg.com/leaflet.markercluster/dist/MarkerCluster.css',
                 'https://unpkg.com/leaflet.markercluster/dist/MarkerCluster.Default.css',
+                // 新增 Leaflet 預設圖示
                 'https://unpkg.com/leaflet/dist/images/marker-icon.png',
                 'https://unpkg.com/leaflet/dist/images/marker-icon-2x.png',
                 'https://unpkg.com/leaflet/dist/images/marker-shadow.png',
+                // 新增紅色標記圖示
                 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
                 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png'
             ]);
@@ -36,9 +24,37 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    // 移除針對離線模式的快取機制，直接嘗試網路請求
+    const url = new URL(event.request.url);
+    
+    // 特別處理 OpenStreetMap 圖磚請求
+    if (url.hostname === 'tile.openstreetmap.org') {
+        event.respondWith(
+            caches.open('map-tiles-cache').then((cache) => {
+                return cache.match(event.request).then((response) => {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request).then((response) => {
+                        if (response.status === 200) {
+                            cache.put(event.request, response.clone());
+                        }
+                        return response;
+                    });
+                });
+            })
+        );
+        return;
+    }
+
+    // 處理其他請求
     event.respondWith(
-        fetch(event.request)
+        caches.match(event.request).then((response) => {
+            if (response) return response;
+            return fetch(event.request).catch((error) => {
+                console.error('PWA fetch failed:', error);
+                throw error;
+            });
+        })
     );
 });
 
